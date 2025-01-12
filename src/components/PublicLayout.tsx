225ed3eed18d83093d9393605dc266f485d96b26
aren talb <aren.talb00@gmail.com>
2025-01-12 17:18:03 +0300

    Refactor file and folder structure

--- src/components/PublicLayout.tsx
+++ src/components/PublicLayout.tsx
@@ -3,11 +3,9 @@ import { Outlet } from "react-router-dom";
 export function PublicLayout() {
   return (
     <div>
-      <header>Public Header</header>
       <main>
         <Outlet />
       </main>
-      <footer>Public Footer</footer>
     </div>
   );
 }
---
+++ src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,7 @@
+export function LoginPage() {
+  return (
+    <div className={"flex justify-center items-center h-screen"}>
+      <div>LoginPage</div>
+    </div>
+  );
+}
