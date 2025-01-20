34035bbfefc369c24db30a2dd74ec7328249300e
aren talb <aren.talb00@gmail.com>
2025-01-21 00:54:09 +0300

    Fix some ui issue in unit to product form

--- src/features/product/forms/CreateUnitForProductForm.tsx
+++ src/features/product/forms/CreateUnitForProductForm.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import {
   useGetProductUnitsDetailQuery,
   useSetUnitToProductMutation,
@@ -7,6 +7,8 @@ import {
 import { useToast } from "@/hooks/use-toast.ts";
 import { ClientError } from "@/app/apiSlice.ts";
 import { Button } from "@/components/ui/button.tsx";
+import { Loader } from "@/components/common/Loader.tsx";
+import { ErrorBox } from "@/components/common/ErrorBox.tsx";
 
 type CreateUnitForProductFormProps = {
   onClose: React.Dispatch<React.SetStateAction<boolean>>;
@@ -16,38 +18,43 @@ export function CreateUnitForProductForm({
   onClose,
 }: CreateUnitForProductFormProps) {
   const { id } = useParams();
-  const productId = Number(id);
+  const productId = Number(id) as number;
 
-  const { data: applicableUnitsData } = useGetProductUnitsDetailQuery(
-    { productId },
-    { skip: !productId },
-  );
+  const {
+    data: applicableUnitsData,
+    isLoading,
+    error,
+  } = useGetProductUnitsDetailQuery({ productId }, { skip: !productId });
 
   const [setUnitToProduct] = useSetUnitToProductMutation();
   const { toast } = useToast();
 
   const [selectedUnits, setSelectedUnits] = useState<number[]>([]);
 
-  const handleUnitClick = useCallback((unitId: number) => {
-    setSelectedUnits((prevSelected) =>
-      prevSelected.includes(unitId)
-        ? prevSelected.filter((id) => id !== unitId)
-        : [...prevSelected, unitId],
-    );
-  }, []);
+  const handleUnitClick = useCallback(
+    (unitId: number) => {
+      setSelectedUnits((prevSelected) =>
+        prevSelected.includes(unitId)
+          ? prevSelected.filter((id) => id !== unitId)
+          : [...prevSelected, unitId],
+      );
+    },
+    [setSelectedUnits],
+  );
 
   const onSubmit = async () => {
+    if (!productId) {
+      toast({ title: "Product ID is invalid", variant: "destructive" });
+      return;
+    }
+
+    if (selectedUnits.length === 0) {
+      toast({ title: "تکایە یەک unit هەڵبژێرە!", variant: "destructive" });
+      return;
+    }
+
     try {
-      if (!productId || selectedUnits.length === 0) {
-        toast({ title: "تکایە یەک unit هەڵبژێرە!", variant: "destructive" });
-        return;
-      }
-
-      await setUnitToProduct({
-        productId,
-        unitIds: selectedUnits,
-      }).unwrap();
-
+      await setUnitToProduct({ productId, unitIds: selectedUnits }).unwrap();
       toast({ title: "سەرکەوتوو بوو" });
 
       setSelectedUnits([]);
@@ -62,41 +69,73 @@ export function CreateUnitForProductForm({
     }
   };
 
+  if (isLoading) return <Loader />;
+  if (error) return <ErrorBox error={error} />;
+
+  const noUnitsToAdd = !applicableUnitsData?.data?.addableUnits?.length;
+
   return (
     <div>
       <div className="flex flex-col gap-2">
-        <div>
-          <p>ئەو یەکانەی لە ئێستایا هەیەتی </p>
-          <div className="flex gap-3">
-            {applicableUnitsData?.data?.currentProductUnits?.map((unit) => (
-              <button className="p-2 border rounded-md" key={unit.id}>
-                {unit.unitSymbol}
-              </button>
-            ))}
+        {applicableUnitsData?.data?.currentProductUnits.length === 0 ? (
+          <p>هیج یەکەیەکی نیە</p>
+        ) : (
+          <div>
+            <p>ئەو یەکانەی لە ئێستایا هەیەتی</p>
+            <div className="flex gap-3">
+              {applicableUnitsData?.data?.currentProductUnits.map((unit) => (
+                <div className="p-2 border rounded-md" key={unit.id}>
+                  {unit.unitSymbol}
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
+        )}
 
-        <div>
-          <p>ئەو یەکانای ئەتوانیت زیادیان بکەیت</p>
-          <div className="flex gap-3">
-            {applicableUnitsData?.data?.addableUnits?.map((unit) => (
-              <button
-                key={unit.id}
-                type="button"
-                onClick={() => handleUnitClick(unit.id)}
-                className={`p-2 border rounded-md ${
-                  selectedUnits.includes(unit.id)
-                    ? "bg-blue-500 text-white"
-                    : "bg-white text-black"
-                }`}
+        {noUnitsToAdd ? (
+          <div className="text-center">
+            <p>هیج یەکەیەکی نیە بتوانیت زیادی بکەیت</p>
+            <p className="text-sm">
+              پێویستە{" "}
+              <Link className="underline text-blue-500" to="/app/units">
+                یەکە
+              </Link>{" "}
+              یان{" "}
+              <Link
+                className="underline text-blue-500"
+                to="/app/unit-conversions"
               >
-                {unit.unitSymbol}
-              </button>
-            ))}
+                پەیوەندی
+              </Link>{" "}
+              هەبێت لەگەڵ یەکە سەرەکیەکەی ئەم کاڵایە
+            </p>
           </div>
-        </div>
+        ) : (
+          <div>
+            <p>ئەو یەکانای ئەتوانیت زیادیان بکەیت</p>
+            <div className="flex gap-3 flex-wrap">
+              {applicableUnitsData?.data?.addableUnits.map((unit) => (
+                <button
+                  key={unit.id}
+                  type="button"
+                  onClick={() => handleUnitClick(unit.id)}
+                  className={`p-2 border rounded-md ${
+                    selectedUnits.includes(unit.id)
+                      ? "bg-blue-500 text-white"
+                      : "bg-white text-black"
+                  }`}
+                  aria-pressed={selectedUnits.includes(unit.id)}
+                >
+                  {unit.unitSymbol}
+                </button>
+              ))}
+            </div>
+          </div>
+        )}
 
-        <Button onClick={onSubmit}>یەکە زیاد بکە</Button>
+        <Button disabled={noUnitsToAdd} onClick={onSubmit}>
+          یەکە زیاد بکە
+        </Button>
       </div>
     </div>
   );
